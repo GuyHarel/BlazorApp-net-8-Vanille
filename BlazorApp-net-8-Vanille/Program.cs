b68f803a137470230efdf75b27f20a02ce1682a6
GuyHarel <guyharel13@gmail.com>
2024-02-20 15:07:26 -0500

    Ajouter package et config pour: prise en charge de l'authentification Azure AD dans votre application

--- BlazorApp-net-8-Vanille/Program.cs
+++ BlazorApp-net-8-Vanille/Program.cs
@@ -1,4 +1,8 @@
 using BlazorApp_net_8_Vanille.Components;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Graph.ExternalConnectors;
+using Microsoft.Identity.Web;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +11,9 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
 
 var app = builder.Build();
 
@@ -17,6 +24,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 
 app.MapControllers();
 app.UseRouting();
